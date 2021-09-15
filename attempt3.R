e679f96405bc8508a952569ec69f927a42b7e5be
smhahmad <90764758+smhahmad@users.noreply.github.com>
2021-09-15 12:52:49 +0100

    Update attempt3.R

--- attempt3.R
+++ attempt3.R
@@ -16,3 +16,16 @@ print(matri)
 > print(matri)
      [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8] [,9] [,10]
 [1,] "B"  "A"  "A"  "B"  "A"  "B"  "A"  "A"  "A"  "B"  
+
+
+test <- c("A", "B")
+matri <- matrix(sample(test, size = 10, replace=T, prob=NULL), ncol=10, nrow=3) #3 rows, could be n rows
+print(matri)
+
+#output
+> matri <- matrix(sample(test, size = 10, replace=T, prob=NULL), ncol=10, nrow=3)
+> print(matri)
+     [,1] [,2] [,3] [,4] [,5] [,6] [,7] [,8] [,9] [,10]
+[1,] "A"  "A"  "A"  "B"  "B"  "A"  "B"  "A"  "B"  "A"  
+[2,] "A"  "B"  "A"  "A"  "A"  "A"  "B"  "B"  "A"  "B"  
+[3,] "B"  "A"  "B"  "A"  "B"  "A"  "A"  "A"  "A"  "B"  
